648a88644effce93d9aaa56f552b2597f9eb90fe
Tomas <69489194+NitsugaSamot@users.noreply.github.com>
2024-02-04 19:08:43 -0300

    añadiendo validaciones de formulario con formik

--- src/pages/new-todo.tsx
+++ src/pages/new-todo.tsx
@@ -4,35 +4,46 @@ import { useRouter } from 'next/router';
 import { doc, collection, addDoc, updateDoc } from 'firebase/firestore';
 import { initializeFirebase } from '../../firebase/firebase';
 import firebaseConfig from '../../firebase/config';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 
 const db = initializeFirebase(firebaseConfig);
 
 const NewTodo = () => {
-  const [idUser, setIdUser] = useState<number | null>(null);
-  const [todo, setTodo] = useState('');
   const [isEditing, setIsEditing] = useState(false);
-  const [id, setId] = useState<string | null>(null);
 
   const router = useRouter();
+
+  const formik = useFormik({
+    initialValues: {
+      idUser: null,
+      todo: '',
+    },
+    validationSchema: Yup.object({
+      idUser: Yup.number().required('El campo ID no puede ir vacío ni debe tener carácteres especiales, debe ser un Número'),
+      todo: Yup.string().required('Debes asignar una actividad en TODOs para completar el Formulario'),
+    }),
+
+    onSubmit: async (values) => {
+      try {
+        await addTodoForFirestore(values);
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  });
 
   useEffect(() => {
     const { query } = router;
     if (query.id && query.userId) {
-      setIdUser(Number(query.userId));
+      formik.setValues({
+        idUser: Number(query.userId),
+        todo: Array.isArray(query.todo) ? query.todo[0] : query.todo,
+      });
       setIsEditing(true);
-  
-      // Usamos una variable temporal para asegurarnos de que cumple con el tipo string | null
-      const newId = Array.isArray(query.id) ? query.id[0] : query.id;
-  
-      setId(newId);
-  
-      const todoValue = Array.isArray(query.todo) ? query.todo[0] : query.todo;
-      if (typeof todoValue === 'string') {
-        setTodo(todoValue);
-      }
     }
   }, [router.query]);
-  
+
   const updateTodoInFirestore = async (id: string, todo: string) => {
     const todoDocRef = doc(db, 'todos', id);
     await updateDoc(todoDocRef, { todo });
@@ -56,7 +67,7 @@ const NewTodo = () => {
           }
         } else {
           // crear
-          const todos = todo.split(',').map(t => t.trim());
+          const todos = todo.split(',').map((t) => t.trim());
           for (const t of todos) {
             await addNewTodoToFirestore(idUser, t);
           }
@@ -64,42 +75,46 @@ const NewTodo = () => {
       } else {
         console.error('ID de usuario no válido');
       }
-  
-      setIdUser(null);
-      setTodo('');
+
+      formik.resetForm();
       setIsEditing(false);
       router.push(`/todos`);
     } catch (error: any) {
       console.error('Error al agregar/actualizar documento:', error);
     }
   };
-  
-
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    await addTodoForFirestore({ id, idUser, todo });
-  };
 
   return (
     <AppLayout>
-      <form
-        onSubmit={handleSubmit}
-        className="bg-white py-10 px-5 md:w-1/2 rounded-lg shadow mt-5 "
-      >
+      <form onSubmit={formik.handleSubmit} className="bg-white py-10 px-5 md:w-1/2 rounded-lg shadow mt-5 ">
         <input
           type="number"
           className="border w-full p-2 mt-2 placeholder-gray-400 rounded-md"
           placeholder="Id del usuario"
-          value={idUser !== null ? idUser : ''}
-          onChange={(e) => setIdUser(e.target.value !== '' ? Number(e.target.value) : null)}
+          {...formik.getFieldProps('idUser')}
         />
+        {formik.touched.idUser && formik.errors.idUser && (
+            <div className="border-red-500 p-3 text-red-700 text-lg mt-1">
+            {formik.touched.idUser && formik.errors.idUser && (
+                <div>{formik.errors.idUser}</div>
+            )}
+            </div>
+        
+        )}
 
         <textarea
           className="border w-full p-2 mt-2 placeholder-gray-400 rounded-md"
           placeholder="Descripción de la tarea"
-          value={todo}
-          onChange={(e) => setTodo(e.target.value)}
+          {...formik.getFieldProps('todo')}
         />
+        {formik.touched.todo && formik.errors.todo && (
+            <div className="border-red-500 p-3 text-red-700 text-lg mt-1">
+            {formik.touched.idUser && formik.errors.todo && (
+              <div>{formik.errors.todo}</div>
+            )}
+          </div>
+          
+        )}
 
         <input
           type="submit"
@@ -112,4 +127,6 @@ const NewTodo = () => {
 };
 
 export default NewTodo;
+
+
 
