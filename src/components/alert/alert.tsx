a12fc775bdc8bfe3b4e888363c727054be88db14
Tomas <69489194+NitsugaSamot@users.noreply.github.com>
2024-02-04 21:32:44 -0300

    añadiendo interface en Alert

--- src/components/alert/alert.tsx
+++ src/components/alert/alert.tsx
@@ -1,12 +1,13 @@
+import React from 'react';
 
-interface Alert {
+interface AlertProps {
   alert: {
     msg: string;
     error: boolean;
   };
 }
 
-const Alert: React.FC<Alert> = ({ alert }) => {
+const Alert: React.FC<AlertProps> = ({ alert }) => {
   return (
     <div className={`${alert.error ? 'from-red-400 to-red-600' : 'from-lime-700 to-lime-900'} bg-gradient-to-br text-center p-3 rounded-xl uppercase text-white font-bold text-sm my-10 `}>
       {alert.msg}
